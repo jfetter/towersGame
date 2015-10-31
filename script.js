8fa2b646f2587beedcdda6c99d4d87932d5a60a4
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-31 08:48:32 -0700

    back to highlighting only

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(document).ready(init);
 function init () {
 
-	var countOfMoves = 0; 
+
 
 	$(".tower").on("click", selectTopOfStack);
  // $("#tower3").on("click", checkForVictory);
 
 }
+
+	var countOfMoves = 0; 
 
 function selectTopOfStack(event){
 			var selectedDisc = $($(this).children()[0]);
@@ -14,41 +16,43 @@ function selectTopOfStack(event){
 			console.log("S D:", selectedDisc, "S D S:", discsDataSize);
 			var lilBroSize = $(this).find(">:nth-child(2)").data("size");
 			console.log("LBS:", lilBroSize);
- 			// if the class "highlight has not been applied to any of the boxes yet then highlight it otherwise dont"
- 			  if  (! $(".floater").hasClass("highlight")){
- 			  		console.log("I am selecting a disc")
-            selectedDisc.addClass("highlight")
-     // but if there already is a highlighted box, then remove the highlight;
+ 			// if the class "highlight has been applied to any of the boxes switch the highlighting
+ 			  if  ( $(".floater").hasClass("highlight")){
+ 						console.log("I deselecting the other disc and selecting this new one")
+ 			  		$(".highlight").removeClass("highlight");
+		  			selectedDisc.addClass("highlight");
+		  			removeOrPlaceDisc;
 		  	} 
-		 // if the class "highlight" has been applied to any of the floating things (ie discs) item when you 
-		 // click a tower, remove the old highlight and put it on the new selection
+		  	// otherwise just add the class highlight ... so there is only ever one highlighted disc
 		  	else {
-		  		console.log("I'd deselecting the other disc and selecting this new one")
-		  		$(".highlight").removeClass("highlight");
-		  		selectedDisc.addClass("highlight")
+		  		$(selectedDisc).addClass("highlight");
+		  		console.log("This is my first highlight")
 		  	}
-		  //but the class removed has not been applied
+		  //if the class removed has not been applied
 		 // if the selected disc is already highlighted
 		 // execute the function to remove selectedDisc from the document (and store it wherever removed things go)
 		 // add the class removed so it now has both highlight and remove and should on third
 		 // click advance to else statement
-		  	if  ( $(selectedDisc).hasClass("highlight") ) {
-		  		console.log("I am removing the disc")
-			 var removedDisc =	$(selectedDisc).addClass("removed").detach();
-		   }	else  {
-		   	countOfMoves ++; 
-		// when selected thing has the class remove and also the class highlight then 
-		// execute the function to append the new tower
-		// with the remove the classes and append it to the tower that was just
-		// clicked ... but only if there is no disc in there already (ie lilBroSize does not
-		// return a truthy value or at least
-		// not already a smaller disc than the
-		// current disc. ... otherwise player will remain in this state (ie have these)
-		// two classes and just keep clicking until they find a tower that fits
-		// within those parameters
- 			if((! lilBroSize || discsDataSize < lilBroSize ))
+		  	function removeOrPlaceDisc () {
+		  	if  ( $(selectedDisc).hasClass("highlight") && $(selectedDisc).hasClass("removed")) {
+		  		console.log("I am placing the disc")
+	 				countOfMoves ++; 
+						// when selected thing has the class remove and also the class highlight then 
+						// execute the function to append the new tower
+						// with the remove the classes and append it to the tower that was just
+						// clicked ... but only if there is no disc in there already (ie lilBroSize does not
+						// return a truthy value or at least
+						// not already a smaller disc than the
+						// current disc. ... otherwise player will remain in this state (ie have these)
+						// two classes and just keep clicking until they find a tower that fits
+						// within those parameters
+ 				if((! lilBroSize || discsDataSize < lilBroSize )){
  				$("this").append(removedDisc);
-		 }
+ 				}
+			} else  {
+ 				var removedDisc =	$(selectedDisc).addClass("removed").detach();   
+ 			}
+ 		}
 };
 
 
