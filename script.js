160896b59f14380dd86a49fda319dfb76f6cf96f
Jillian Fetter <jillian.fetter@gmail.com>
2015-11-01 14:51:26 -0800

    still cannot place blocks but getting closer

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
 $(document).ready(init);
 function init(){
+
 	$(".tower").on("click", selectTopOfStack);
-	$(".contains").on("click", placePiece)
+
 }
 
 function selectTopOfStack (event) {
-	var selectedPiece = event.target.children[0].children[0];
-	if ($(selectedPiece).hasClass("highlight")){
-		$(".highlight").removeClass("highlight")
-		removePiece(selectedPiece);
-	} else {
-		$(".highlight").toggleClass("highlight")
-		$(selectedPiece).addClass("highlight")
+if ($("#tower1").hasClass("removed")){
+	$(".removed").removeClass(".removed")
+		var receivingTower = $(this).find(">:first-child")
+	// if ( || )
+	console.log("I would like to place in" + (receivingTower))
+
+		$(receivingTower).append(removePiece());
+}else {var selectedPiece = $(this).find(">:first-child");
+		selectedPiece = selectedPiece.find(">:first-child");
+		if ($(selectedPiece).hasClass("highlight")){
+			$(".highlight").removeClass("highlight")	
+		console.log("I just un-highlighted a piece yo")
+			removePiece(selectedPiece);	
+		} else {
+			$(".highlight").removeClass("highlight")
+			$(selectedPiece).addClass("highlight")
+		console.log("ELSE I just highlighted a piece yo")
+			return
+		}
 	}
 }
 
 function removePiece (selectedPiece) {
+	console.log("removed the shit outta this piece")
+	$(selectedPiece).addClass("highlight")
+	$(".tower").addClass("removed");
 	var removedPiece = $(selectedPiece).remove();
+	console.log("...so tower1 should have a class of removed now")
 	return removedPiece;
 }
 
-function placePiece (removedpiece) {
+function placePiece(selectedTower){
+		$(".removed").removeClass(".removed")
+		var receivingTower = $(this).find(">:first-child")
 	// if ( || )
-	$(this).append(removedPiece);
+	console.log("I would like to place in" + selectedTower)
+
+		$(selectedTower).append(removePiece());
 }
+
 
 //for calc -> $().on("keypress", "MAIN OPERATOR IF ELSE FUNCTION"
 // || keypress.(Charcode("ASCII KEY CODE HERE"))
